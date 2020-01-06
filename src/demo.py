bb75f91a8db9c0e9923fa04d09cb5d9177ec230d
hyperparameters <tushrcool.89@gmail.com>
2020-01-06 16:30:34 +0530

    demo modified

--- src/demo.py
+++ src/demo.py
@@ -9,6 +9,8 @@ import cv2
 
 from opts import opts
 from detectors.detector_factory import detector_factory
+detection_json = dict()
+outpath = "results"
 
 image_ext = ['jpg', 'jpeg', 'png', 'webp']
 video_ext = ['mp4', 'mov', 'avi', 'mkv']
@@ -45,12 +47,64 @@ def demo(opt):
     else:
       image_names = [opt.demo]
     
+    #find input directory
+    inpath = os.path.dirname(image_names[0])
+
+    #create output at same directory
+    outpath = outpath + inpath
+    if not os.path.isdir(outpath):
+      os.makedirs(outpath)
+      
     for (image_name) in image_names:
       ret = detector.run(image_name)
+      result = ret["results"]
+      image = cv2.imread(image_name)
+      name = os.path.basename(image_name).split(".")[0]
+      
+      create_json(result,name)
+      image = create_bbox(result,image
+      save_image(image,name)
+      
+      
       time_str = ''
       for stat in time_stats:
         time_str = time_str + '{} {:.3f}s |'.format(stat, ret[stat])
       print(time_str)
+
+    save_json()
+
+def create_bbox(result,image):
+  if len(show_class)==0:
+    show_class = range(1,opt.num_classes)  
+  for i in show_class:
+    for bbox in result[i]:
+      if bbox[4]>= opt.vis_thresh:
+        cv2.rectangle(image,(bbox[0],bbox[1]),(bbox[2],bbox[3]),color[i],2)
+  return image
+  
+def save_image(image,name):
+  cv2.imwrite(os.path.join(outpath,name+".jpg"),image)
+  print("imaged saved : {}".format(name))
+
+def create_json(result,name):
+
+  if len(show_class)==0:
+    show_class = range(1,opt.num_classes)
+  det = []  
+  for i in show_class:
+    for bbox in result[i]:
+      det.append({bbox[4] : bbox[:4].tolist()})
+
+  #add detections to dictionary
+  detections_json[name] = det
+ 
+def save_json():
+  with open("detections.json","w") as f:
+    f.write(json.dumps(detections_json))
+    print("json results saved")
+    
 if __name__ == '__main__':
   opt = opts().init()
+  if not os.isdir(outpath):
+    os.path.mkdir(outpath)
   demo(opt)
